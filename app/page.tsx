45a1fb5fd4c75b83361d40ab11ac783562782f7d
ausin107 <nguyenphuqui396@gmail.com>
2024-05-14 21:45:10 +0700

    indoor air quality create

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,65 @@
-import Image from "next/image";
-
+import Image from 'next/image'
+import Sun from '@/app/assets/sun.png'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main className='px-16 pt-10'>
+      <div className='lg:mb-10 md:mb-5'>
+        <h1 className='lg:text-4xl md:text-lg text-base font-bold mb-5'>Indoor air quality at Home</h1>
+        <p className='lg:text-lg md:text-base mb-3 font-semibold'>Air quality index (AQI) and PM2.5 air pollution</p>
+        <p className='text-sm'>
+          <strong>Last update:</strong> 10s ago
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </div>
+      <div className='flex'>
+        <div className='flex flex-col items-center lg:w-72 lg:px-8 lg:py-5 border shadow mr-12'>
+          <div className='flex items-center justify-between w-full mb-5'>
+            <h1 className='text-lg font-bold'>Home</h1>
+            <Image src={Sun} alt='Image' className='w-16' />
+          </div>
+          <p className='mb-5'>Today, 13 May</p>
+          <div className='mb-5 relative'>
+            <span className='font-bold text-7xl'>28</span>
+            <span className='text-4xl absolute top-0 -right-5'>°</span>
+          </div>
+          <p className='mb-3'>Humidity: 50%</p>
+          <p className='mb-5'>Temperature: 28°C</p>
+        </div>
+        <div className='flex flex-col flex-grow'>
+          <div className='flex bg-[#A8E05F] px-12 py-10 rounded'>
+            <div className='bg-[#87C13C] px-3 py-5 rounded w-28 h-28 mr-10'>
+              <p className='text-white text-sm'>HOME AQI</p>
+              <p className='text-white font-semibold text-3xl items-end'>20</p>
+            </div>
+            <div className='pt-5'>
+              <p className='text-[#607631] text-sm mb-2 font-medium'>LIVE AQI INDEX</p>
+              <p className='text-[#607631] text-4xl font-bold'>Good</p>
+            </div>
+          </div>
+          <div className='px-12 py-5 border-x border-b shadow'>
+            <div className='flex bg-[#E1DD72] py-5 px-7 rounded w-full justify-between items-center mb-5'>
+              <p className='font-bold text-2xl'>PM2.5</p>
+              <div className='w-36'>
+                <span className='font-bold text-4xl mr-3'>40</span>
+                <span className='font-semibold'>µg/m³</span>
+              </div>
+            </div>
+            <div className='flex bg-[#A8C66C] py-5 px-7 rounded w-full justify-between items-center mb-5'>
+              <p className='font-bold text-2xl'>ECO2</p>
+              <div className='w-36'>
+                <span className='font-bold text-4xl mr-3'>55</span>
+                <span className='font-semibold'>ppb</span>
+              </div>
+            </div>
+            <div className='flex bg-[#24B589] py-5 px-7 rounded w-full justify-between items-center'>
+              <p className='font-bold text-2xl'>TVOC</p>
+              <div className='w-36'>
+                <span className='font-bold text-4xl mr-3'>450</span>
+                <span className='font-semibold'>ppm</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  );
+  )
 }
